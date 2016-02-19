aff833326823bdd4460a38ce8ab11e091bf85393
moverperfect <moverperfect@gmail.com>
2016-02-19 16:36:59 +0000

    Added comments to ExcelColumn

--- ExtractFromSharepoint/ExcelColumn.cs
+++ ExtractFromSharepoint/ExcelColumn.cs
@@ -1,21 +1,35 @@
 namespace ExtractFromSharepoint
 {
+    /// <summary>
+    /// Represents one column object in excel
+    /// </summary>
     internal class ExcelColumn
     {
+        /// <summary>
+        /// Empty column constructor
+        /// </summary>
         internal ExcelColumn()
         {
             Name = "";
             Width = 0;
         }
 
+/*
         internal ExcelColumn(string name, int width)
         {
             Name = name;
             Width = width;
         }
+*/
 
+        /// <summary>
+        /// The name of the column
+        /// </summary>
         internal string Name { get; set; }
 
+        /// <summary>
+        /// The width to set the column too
+        /// </summary>
         internal decimal Width { get; set; }
     }
 }
