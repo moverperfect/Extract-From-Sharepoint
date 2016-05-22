b34586908d934c23e4bb035439d36c01a1082d89
moverperfect <moverperfect@gmail.com>
2016-05-22 19:26:01 +0100

    Added support for tabs in sharepoint Added validation for links Added detection for tables inside attributes that may hurt the program

--- ExtractFromSharepoint/AppDetail.cs
+++ ExtractFromSharepoint/AppDetail.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using OpenQA.Selenium;
 
 namespace ExtractFromSharepoint
@@ -22,40 +23,76 @@ namespace ExtractFromSharepoint
             ProperyNames = new List<string>();
             Properties = new List<string>();
 
-            // Grab all of the rows from the page and iterate over them
-            var rows = ieDriver.FindElement(By.ClassName("ms-formtable")).FindElements(By.TagName("tr"));
-            foreach (var row in rows)
+            // Grab the form tables from the page
+            var formtables = ieDriver.FindElements(By.ClassName("ms-formtable"));
+            var tabElements = ieDriver.FindElements(By.ClassName("ui-tabs-anchor"));
+
+            if (formtables.Count - 1 != tabElements.Count)
             {
-                // Grab the 2 cells from each row
-                var cells = row.FindElements(By.TagName("td"));
+                Console.WriteLine("WARNING! We have detected that you have invisible tables that may mess up the program");
+                Console.WriteLine("We will try to detect them and work around them but this may not work");
+            }
 
-                // If the title is empty then continue
-                if(cells[0].Text == "")
-                    continue;
+            // Used to blacklist formtables so that we do not loop over them
+            var blacklist = new List<int>();
 
-                // Grab the property name, property and any links that are within the property
-                var propName = cells[0].Text;
-                var links = cells[1].FindElements(By.TagName("a"));
-                var prop = cells[1].Text;
-
-                // For all of the links that were discovered
-                foreach (var link in links)
+            for (var i = 0; i < formtables.Count - 1; i++)
+            {
+                var formtable = formtables[i];
+                if (formtable.FindElements(By.ClassName("ms-formtable")).Count > 0)
                 {
-                    // Get the href of the link
-                    var href = link.GetAttribute("href");
-
-                    // If the href is an external site then continue
-                    if(!href.Contains(Program.Domian))
-                        continue;
-
-                    // Add in a representation of the link and the name of the link
-                    prop = prop.Replace(link.Text,
-                        "||(|)||" + href + "||()||" + link.Text + "||(|)||");
+                    Console.WriteLine("Invisible table has been found inside tab " + (i+1));
+                    blacklist.Add(i+1);
                 }
 
-                // Add the properties to the object
-                ProperyNames.Add(propName);
-                Properties.Add(prop);
+                // If this table is in the blacklist then continue over it
+                if (blacklist.Contains(i))
+                {
+                    continue;
+                }
+
+                tabElements[i].Click();
+                tabElements[i].Click();
+                // Grab all of the rows from the form page and iterate over them
+                var rows = formtable.FindElements(By.TagName("tr"));
+                foreach (var row in rows)
+                {
+                    // Grab the 2 cells from each row
+                    var cells = row.FindElements(By.TagName("td"));
+
+                    // If the title is empty then continue or if the 'title' does not contain 'ms-formlabel' as a class then it is not a title
+                    if (cells[0].Text == "" || !cells[0].GetAttribute("class").Contains("ms-formlabel"))
+                        continue;
+
+                    // Grab the property name, property and any links that are within the property
+                    var propName = cells[0].Text;
+                    var links = cells[1].FindElements(By.TagName("a"));
+                    var prop = cells[1].Text;
+
+                    // For all of the links that were discovered
+                    foreach (var link in links)
+                    {
+                        // Get the href of the link
+                        var href = link.GetAttribute("href");
+
+                        var unspupportedRegex =
+                            new Regex(
+                                "(^(PRN|AUX|NUL|CON|COM[1-9]|LPT[1-9]|(\\.+)$)(\\..*)?$)|(([\\x00-\\x1f\\\\?*:\";‌​|/<>‌​])+)",
+                                RegexOptions.IgnoreCase);
+
+                        // If the href is an external site then continue
+                        if (!href.Contains(Program.Domian) || unspupportedRegex.IsMatch(link.Text))
+                            continue;
+
+                        // Add in a representation of the link and the name of the link
+                        prop = prop.Replace(link.Text,
+                            "||(|)||" + href + "||()||" + link.Text + "||(|)||");
+                    }
+
+                    // Add the properties to the object
+                    ProperyNames.Add(propName);
+                    Properties.Add(prop);
+                }
             }
         }
 
